9f11cebe6de08efdae8a50ad6a9191a940d0aac0
hamunna <hamunna@gmail.com>
2021-11-01 12:46:03 +0600

    SylTour Server-side Started

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const cors = require('cors');
+require('dotenv').config();
 const app = express();
 const port = 5000;
 
@@ -8,11 +9,11 @@ const port = 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-	console.log('You got the access.');
-	res.send('Server is Running...');
+app.get('/', (res, req) => {
+	console.log('Server Running Successfully!')
+	res.send('Server is Running...')
 });
 
 app.listen(port, () => {
-	console.log('Server is running on port: ', port);
-})
+	console.log('Server is Running on port: ', port);
+});
