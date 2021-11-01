86503f9def06c9e55452510d2c33b291a85a8a15
hamunna <hamunna@gmail.com>
2021-11-01 13:34:54 +0600

    Initial Setup Done

--- index.js
+++ index.js
@@ -5,9 +5,43 @@ require('dotenv').config();
 const app = express();
 const port = 5000;
 
+// user: sylTour
+// password: AbNHMWFCkOhwd0GW
+
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xribv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("sylTour");
+    const toursCollection = database.collection("tours");
+
+    const cursor = toursCollection.find(query, options);
+
+    // print a message if no documents were found
+    if ((await cursor.count()) === 0) {
+      console.log("No documents found!");
+    }
+
+    // replace console.dir with your callback to access individual elements
+    await cursor.forEach(console.dir);
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 
 app.get('/', (res, req) => {
 	console.log('Server Running Successfully!')
